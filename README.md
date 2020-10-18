7881fda47ea3ec84d9a88132fca4dc11f93b5846
markshelgren <67986616+markshelgren@users.noreply.github.com>
2020-10-18 12:10:24 -0700

    Upload final version pending changes from User testing

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 #05 Third Party APIs: Work Day Scheduler
 
-This is a very simple "Work" Day planner.
+This is a very simple "Work" Day planner. It is being submitted in response to Homework Assignment 4
 
 It uses "moment" javascript for date and time processing and allows a user to enter events into hourly buckets that are then saved to local storage to remain persistent.
+
+The current hour of the current day is determined and used to colorize the input hourly boxes. Boxes for hours before the current hour are colorized with a background color of gray. Boxes for hours after the current hourt are coloized with a background color of green. The current hour input box is colorized with a background color of red.
+
+The events and activities entered into the hourly boxes are saved using local storage and are retrieved when the calculator is started.
+
+If the requirements were any more sophisticated time would have been invested using "daily" objects with 24 hours and the application would use significantly more array processing than has been engineered into this very simple version.
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,73 +1,73 @@
 
 $(document).ready(function () {
 
-  //Setting up the date and appending to the HTML page - using moment
-  var today = moment().format('MMMM Do YYYY');
-  console.log(today)
-  $(".todaysDate").append(today);
-
-  var now = parseInt(moment().format('HH'));
-  console.log(now)
-
-  // variables for button ids 
-  var $text9AM = $("#text9AM");
-  var $text10AM = $("#text10AM");
-  var $text11AM = $("#text11AM");
-  var $text12AM = $("#text12AM");
-  var $text1PM = $("#text1PM");
-  var $text2PM = $("#text2PM");
-  var $text3PM = $("#text3PM");
-  var $text4PM = $("#text4PM");
-  var $text5PM = $("#text5PM");
-
-
-  $("textarea").each(function () {
-      var name = parseInt($(this).attr("name"));
-      console.log("Name: " + name);
-      console.log("Now: " + 
-      now)
-      if (name < now) {
-          $(this).addClass("bg-gray");
-      }
-
-
-      if (name > now) {
-          $(this).addClass("bg-green")
-      }
-
-      if (name === now) {
-          $(this).addClass("bg-red")
-      }
-
-
-
-  })
-
-  // Save to local storage 
-  $("button").on("click", function () {
-
-      //setting items in the local storage
-      localStorage.setItem("9AM", ($text9AM.val()))
-      localStorage.setItem("10AM", ($text10AM.val()))
-      localStorage.setItem("11AM", ($text11AM.val()))
-      localStorage.setItem("12AM", ($text12AM.val()))
-      localStorage.setItem("13PM", ($text1PM.val()))
-      localStorage.setItem("14PM", ($text2PM.val()))
-      localStorage.setItem("15PM", ($text3PM.val()))
-      localStorage.setItem("16PM", ($text4PM.val()))
-      localStorage.setItem("17PM", ($text5PM.val()))
-
-  })
-
-  //retreive the stored content from local storage 
-  $("#text9AM").append(localStorage.getItem("9AM"));
-  $("#text10AM").append(localStorage.getItem("10AM"));
-  $("#text11AM").append(localStorage.getItem("11AM"));
-  $("#text12AM").append(localStorage.getItem("12AM"));
-  $("#text1PM").append(localStorage.getItem("13PM"));
-  $("#text2PM").append(localStorage.getItem("14PM"));
-  $("#text3PM").append(localStorage.getItem("15PM"));
-  $("#text4PM").append(localStorage.getItem("16PM"));
-  $("#text5PM").append(localStorage.getItem("17PM"));
-
-})
+    //Setting up the date and appending to the HTML page - using moment
+    var today = moment().format('MMMM Do YYYY');
+    console.log(today)
+    $(".todaysDate").append(today);
+  
+    var now = parseInt(moment().format('HH'));
+    console.log(now)
+  
+    // variables for button ids 
+    var $hour9AM = $("#hour9AM");
+    var $hour10AM = $("#hour10AM");
+    var $hour11AM = $("#hour11AM");
+    var $hour12AM = $("#hour12AM");
+    var $hour1PM = $("#hour1PM");
+    var $hour2PM = $("#hour2PM");
+    var $hour3PM = $("#hour3PM");
+    var $hour4PM = $("#hour4PM");
+    var $hour5PM = $("#hour5PM");
+  
+  // Each hourly html cell is defined with a text area class that enables this function to be executed for all 10 hourly cells
+    $("textarea").each(function () {
+        var name = parseInt($(this).attr("name"));
+        console.log("Name: " + name);
+        console.log("Now: " + 
+        now)
+        if (name < now) {
+            $(this).addClass("bg-gray");
+        }
+  
+  
+        if (name > now) {
+            $(this).addClass("bg-green")
+        }
+  
+        if (name === now) {
+            $(this).addClass("bg-red")
+        }
+  
+  
+  
+    })
+  
+    // Save to local storage 
+    $("button").on("click", function () {
+  
+        //setting items in the local storage
+        localStorage.setItem("9AM", ($hour9AM.val()))
+        localStorage.setItem("10AM", ($hour10AM.val()))
+        localStorage.setItem("11AM", ($hour11AM.val()))
+        localStorage.setItem("12AM", ($hour12AM.val()))
+        localStorage.setItem("13PM", ($hour1PM.val()))
+        localStorage.setItem("14PM", ($hour2PM.val()))
+        localStorage.setItem("15PM", ($hour3PM.val()))
+        localStorage.setItem("16PM", ($hour4PM.val()))
+        localStorage.setItem("17PM", ($hour5PM.val()))
+  
+    })
+  
+    //retreive the stored content from local storage 
+    $("#hour9AM").append(localStorage.getItem("9AM"));
+    $("#hour10AM").append(localStorage.getItem("10AM"));
+    $("#hour11AM").append(localStorage.getItem("11AM"));
+    $("#hour12AM").append(localStorage.getItem("12AM"));
+    $("#hour1PM").append(localStorage.getItem("13PM"));
+    $("#hour2PM").append(localStorage.getItem("14PM"));
+    $("#hour3PM").append(localStorage.getItem("15PM"));
+    $("#hour4PM").append(localStorage.getItem("16PM"));
+    $("#hour5PM").append(localStorage.getItem("17PM"));
+  
+  })    
